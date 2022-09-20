b55e8513ee95cf0e9f957a57eb34fdf1e744f5fb
ch <ch@templatemonster.me>
2022-09-20 11:58:08 +0300

    Added check for mongodb, rabbitmq, elasticsearch. Added check urls by promise.

--- src/healthchecks/Module.php
+++ src/healthchecks/Module.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * @author Alexander Stepanenko <alex.stepanenko@gmail.com>
  * @package templatemonster\healthchecks
  */
 
 namespace templatemonster\healthchecks;
 
 use Yii;
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise\Utils;
+use GuzzleHttp\Psr7\Response;
 use templatemonster\healthchecks\models\HealthCheck;
 
 class Module extends \yii\base\Module
@@ -19,21 +21,26 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
-        foreach ($this->checks as $key=>$val) {
+        foreach ($this->checks as $key => $val) {
+            $args = null;
             if (!is_callable($val)) {
-                $key = $val;
-                $val = [$this, 'check'.ucfirst($key)];
+                if (!is_array($val)) {
+                    $key = $val;
+                } else {
+                    $args = $val;
+                }
+                $val = [$this, 'check' . ucfirst($key)];
             }
-            $this->addHealthCheck($key, $val);
+            $this->addHealthCheck($key, $val, $args);
         }
     }
 
-    public function addHealthCheck($name, $callback)
+    public function addHealthCheck($name, $callback, $args = null)
     {
         if (!is_callable($callback)) {
             throw new \InvalidArgumentException('Health check must be callable');
         }
-        $this->healthCheks[$name] = $callback;
+        $this->healthCheks[$name] = ['callback' => $callback, 'args' => $args];
     }
 
     public function getHealth()
@@ -45,15 +52,31 @@ class Module extends \yii\base\Module
     {
         $this->health = true;
         $result = [];
-        foreach ($this->healthCheks as $name=>$callback) {
-            $check = call_user_func($callback);
-            if (!$check) {
-                $this->health = false;
+        foreach ($this->healthCheks as $name => $callbackData) {
+            if (!empty($callbackData['args'])) {
+                $checkResult = call_user_func($callbackData['callback'], $callbackData['args']);
+            } else {
+                $checkResult = call_user_func($callbackData['callback']);
             }
-            $model = new HealthCheck;
-            $model->name = $name;
-            $model->passed = $check;
-            $result[] = $model;
+            if (is_array($checkResult)) {
+                foreach ($checkResult as $checkName => $check) {
+                    if (!$check) {
+                        $this->health = false;
+                    }
+                    $model = new HealthCheck;
+                    $model->name = $checkName;
+                    $model->passed = $check;
+                    $result[] = $model;
+                }
+            } else {
+                if (!$checkResult) {
+                    $this->health = false;
+                }
+                $model = new HealthCheck;
+                $model->name = $name;
+                $model->passed = $checkResult;
+                $result[] = $model;
+            }
         }
         return $result;
     }
@@ -67,6 +90,7 @@ class Module extends \yii\base\Module
                 return true;
             }
         } catch (\Exception $e) {
+            Yii::error($e);
         }
         return false;
     }
@@ -77,7 +101,71 @@ class Module extends \yii\base\Module
             $cache = Yii::$app->cache;
             return $cache->set('healthcheck', 1);
         } catch (\Exception $e) {
+            Yii::error($e);
         }
         return false;
+    }
+
+    public function checkMongodb()
+    {
+        try {
+            /** @var \yii\mongodb\Connection $connection */
+            $connection = \Yii::$app->mongodb;
+            $connection->open();
+            return $connection->getIsActive();
+        } catch (\Exception $e) {
+            Yii::error($e);
+            return false;
+        }
+    }
+
+    public function checkRabbitmq()
+    {
+        try {
+            return \Yii::$app->queue
+                ->getContext()
+                ->getLibChannel()
+                ->is_open();
+        } catch (\Exception $e) {
+            Yii::error($e);
+            return false;
+        }
+    }
+
+    public function checkElasticsearch()
+    {
+        try {
+            /** @var \yii\elasticsearch\Connection $connection */
+            $connection = \Yii::$app->elasticsearch;
+            $connection->open();
+            if ($connection->getIsActive()) {
+                return true;
+            }
+        } catch (\Exception $e) {
+            Yii::error($e);
+        }
+        return false;
+    }
+
+    public function checkUrls(array $urls)
+    {
+        $client = new Client([]);
+        $promises = [];
+        $checks = [];
+        foreach ($urls as $name => $url) {
+            $checks[$name] = false;
+            try {
+                $promises[$name] = $client->getAsync($url);
+            } catch (\Throwable $e) {
+                Yii::error($e);
+            }
+        }
+        $responses = Utils::settle($promises)->wait();
+        foreach ($responses as $name => $item) {
+            /** @var Response $response */
+            $response = $item['value'] ?? null;
+            $checks[$name] = $response !== null && $response->getStatusCode() === 200;
+        }
+        return $checks;
     }
 }
